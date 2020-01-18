b31ec209c361c9aae54f30c32b2a4140b71ce2d3
Moj <maghrout@googlemail.com>
2020-01-18 13:01:32 +0000

    Fix third test to pass for buzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,6 +1,8 @@
 const fizzBuzz = function fizzBuzz(number) {
     if(number % 2 === 0) {
         return "Fizz";
+    } else if (number % 3 === 0) {
+        return "Buzz";
     }
     return "";
 }
