b5b41aa1d589216bb299914d431cd99d954fa74d
Moj <maghrout@googlemail.com>
2020-01-18 13:05:20 +0000

    add and fix fourth test for FizzBuzz condition

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,8 +1,10 @@
 const fizzBuzz = function fizzBuzz(number) {
-    if(number % 2 === 0) {
-        return "Fizz";
+    if(number % 2 === 0 && number % 3 === 0) {
+        return "FizzBuzz";
     } else if (number % 3 === 0) {
         return "Buzz";
+    } else if (number % 2 === 0) {
+        return "Fizz";
     }
     return "";
 }
--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -7,15 +7,21 @@ describe('Fizz Buzz Solver', () => {
         expect(fizzBuzz(11)).toEqual("");
     })
 
-    it('Should return Fizz if number is divisible by 2', () => {
+    it('Should return Fizz if number is divisible by 2 but not 3', () => {
         expect(fizzBuzz(2)).toEqual("Fizz");
-        expect(fizzBuzz(6)).toEqual("Fizz");
-        expect(fizzBuzz(12)).toEqual("Fizz");
+        expect(fizzBuzz(8)).toEqual("Fizz");
+        expect(fizzBuzz(4)).toEqual("Fizz");
     })
 
-    it('Should return Buzz if number is divisible by 3', () => {
+    it('Should return Buzz if number is divisible by 3 but not 2', () => {
         expect(fizzBuzz(3)).toEqual("Buzz");
         expect(fizzBuzz(9)).toEqual("Buzz");
         expect(fizzBuzz(15)).toEqual("Buzz");
+    })
+
+    it('Should return FizzBuzz if number is divisible by 2 and 3', () => {
+        expect(fizzBuzz(6)).toEqual("FizzBuzz");
+        expect(fizzBuzz(12)).toEqual("FizzBuzz");
+        expect(fizzBuzz(24)).toEqual("FizzBuzz");
     })
 })
